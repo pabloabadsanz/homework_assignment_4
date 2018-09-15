513dff4af3d5d4cba0aebb61acf05456a20a4662
pabloabadsanz <pabloabadsanz@gmail.com>
2018-09-15 17:17:23 +0200

    Created the man page

--- app/lib/pizzacli.js
+++ app/lib/pizzacli.js
@@ -52,12 +52,87 @@ pizzacli.responders = {};
 
 // Help / man responder
 pizzacli.responders.help = function() {
-  console.log("You asked for help");
+  var commands = {
+    'exit': 'Kill the CLI (and the rest of the application)',
+    'man': 'Show this help page',
+    'help': 'Alias of the "man" command',
+    'menu': 'List all the ingredients in the menu',
+    'list orders --{hours}': 'Show the list of all the orders placed in the last {hours}, default to 24h',
+    'lookup order --{orderId}': 'Show details of a specific order',
+    'list users --{hours}': 'Show the list of all the users who signed up in the last {hours}, default to 24h',
+    'lookup user --{userId}': 'Show details of a specific user'
+  };
+
+  // Show a header for the help page that is as wide as the screen
+  pizzacli.addHorizontalLine();
+  pizzacli.centerText('PIZZA CLI MANUAL PAGE');
+  pizzacli.addHorizontalLine();
+  pizzacli.addVerticalSpace(2);
+
+  // Show each command, followed by its explanation, in white respectively
+  for(var key in commands) {
+    if (commands.hasOwnProperty(key)) {
+      var value = commands[key];
+      var line = '\x1b[33m' + key + '\x1b[0m';
+      var padding = 60 - line.length;
+      for (i = 0; i < padding; i++) {
+        line += ' ';
+      }
+      line += value;
+
+      console.log(line);
+      pizzacli.addVerticalSpace();
+    }
+  }
+
+  pizzacli.addVerticalSpace();
+  pizzacli.addHorizontalLine();
+
+};
+
+// Create a vertical space
+pizzacli.addVerticalSpace = function(lines) {
+  lines = typeof(lines) == 'number' && lines > 0 ? lines : 1;
+  for (i = 0; i < lines; i++) {
+    console.log('');
+  }
+};
+
+// Create a horizontal line
+pizzacli.addHorizontalLine = function() {
+  // Get the available screen size
+  var width = process.stdout.columns;
+
+  var line = '';
+  for (i = 0; i < width; i++) {
+    line += '-';
+  }
+
+  console.log(line);
+};
+
+// Create centered text on the screen
+pizzacli.centerText = function(str) {
+  str = typeof(str) == 'string' && str.trim().length > 0 ? str.trim() : '';
+
+  // Get the available screen size
+  var width = process.stdout.columns;
+
+  // Calculate the left padding
+  var leftPadding = Math.floor((width - str.length) / 2);
+
+  // Put in left padded spces before the string
+  var line = '';
+  for (i = 0; i < leftPadding; i++) {
+    line += ' ';
+  }
+  line += str;
+  console.log(line);
 };
 
 // Exit
 pizzacli.responders.exit = function() {
-  console.log("You asked for exit");
+  process.exit(0);
 };
 
 // Show the ingredients in the menu
